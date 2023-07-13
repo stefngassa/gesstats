8734aecca54d3b462ae6efdec558024ce2ebbee4
Ngassa Stephane <38215900+stefngassa@users.noreply.github.com>
2023-07-13 06:17:57 +0100

    Modify buil_files

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,5 @@
 echo "Building the project"
 pip install -r requirements.txt
 
-echo "Make migrations"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
 echo "Collect static"
 python3.9 manage.py collectstatic --noinput --clear
